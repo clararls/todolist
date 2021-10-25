374c77d214f1246a27d74748e07315e6724b1920
Clara Rojano León-Salas <clararojanols@gmail.com>
2021-10-25 14:51:11 +0000

    complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,67 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [Task, setTask] = useState("");
+	const [List, setList] = useState([]);
+	const delTask = index => {
+		let setInput = List.filter((Task, i) => i !== index);
+		setList(setInput);
+	};
+	const addTask = e => {
+		if (e.keyCode === 13) {
+			setTask(Task);
+			let listTask = List.concat({ label: Task, done: false });
+			setList(listTask);
+			setTask("");
+		}
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-3">
+			<h1>Todo List</h1>
+			<input
+				type="text"
+				value={Task}
+				placeholder="What needs to be done?"
+				onChange={e => setTask(e.target.value)}
+				onKeyUp={addTask}
+			/>
+			<div id="listbar">
+				{List.map((todo, i) => (
+					<>
+						<div>
+							{todo.label}
+							<button
+								className="btn btn-danger"
+								id="delete"
+								onClick={() => delTask(i)}>
+								<i
+									className="fas fa-trash-alt"
+									aria-hidden="true"></i>
+							</button>
+						</div>
+					</>
+				))}
+				<div className="row">
+					<div className="col mx-auto">
+						<div className="list-group">
+							{List.length == 0 ? (
+								<div className="list-group-item">
+									Add your task...
+								</div>
+							) : (
+								""
+							)}
+							<li
+								id="last"
+								className="list-group text-muted text-start counter">
+								{List.length} Items left
+							</li>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
-
 export default Home;
